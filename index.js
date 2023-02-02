815bd74d5b156ed1c9fad375ca5e5674edf5820b
Carl_Javier <ccjavier87@gmail.com>
2023-02-02 01:51:15 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let resp;
+  if (feet <= 400) {
+    resp = 'This one is on me!';
+  } else if (feet > 400 && feet <= 2000) {
+    resp = 'That will be twenty bucks.';
+  } else if (feet > 2000 && feet <= 2500) {
+    resp = 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    resp = 'No can do.';
+  }
+  return resp;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city == "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let thanks;
+  switch (tip) {
+    case 'generous' : 
+      return thanks = "Thank you so much.";
+      break;
+    case 'not as generous' :
+      return thanks = "Thank you.";
+      break;
+    default:
+      return thanks = "Bye.";
+      break;
+
+  }
+
 }
